3451fb1a622448760368ef0809ae725d2c9d4ae7
adriano328 <lucasadrianodias@gmail.com>
2022-11-10 22:14:47 -0400

    Envio de arquivos

---
+++ src/main/java/br/com/apirest/apirestjunit/ApiRestJunitApplication.java
@@ -0,0 +1,13 @@
+package br.com.apirest.apirestjunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestJunitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestJunitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/apirest/apirestjunit/ApiRestJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apirest.apirestjunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestJunitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
